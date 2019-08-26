ff1666a6fbc519f100db1b4c1d46efdc77bc43b0
Ayush Pratap Singh <51674131+AyushSingh004@users.noreply.github.com>
2019-08-27 02:42:27 +0530

    Update Data_preprossing_1.py
    DATA_preprocessing of a datasets,

--- Data_preprossing_1.py
+++ Data_preprossing_1.py
@@ -1,7 +1,9 @@
+#importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing datasets
 dataset=pd.read_csv('Data.csv')
 
 X=dataset.iloc[ : , 0:3 ].values
@@ -9,11 +11,37 @@ X=dataset.iloc[ : , 0:3 ].values
 y=dataset.iloc[ : , -1].values
 #y=pd.DataFrame(y)
 
+#Taking care of missing data
 from sklearn.impute import SimpleImputer
 sim=SimpleImputer()
 sim.fit(X[:,1:3])
 X[:, 1:3]=sim.transform(X[:,1:3])
 X
+#Encoding categorial variable
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+X[:, 0]=le.fit_transform(X[: , 0])
+X
+
+#Dumy Encoding using OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder=OneHotEncoder(categorical_features= [0])
+X=onehotencoder.fit_transform(X).toarray()
+
+#for y
+from sklearn.preprocessing import LabelEncoder
+le1 = LabelEncoder()
+y= le1.fit_transform(y)
+
+
+
+
+
+
+
+
+
+
 
 
 
