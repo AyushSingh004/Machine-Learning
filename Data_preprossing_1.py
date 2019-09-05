cfd6030c7b03ed5cb9e8766d3886b672765102ad
Ayush Pratap Singh <51674131+AyushSingh004@users.noreply.github.com>
2019-09-05 23:43:43 +0530

    Update Data_preprossing_1.py

--- Data_preprossing_1.py
+++ Data_preprossing_1.py
@@ -28,7 +28,20 @@ from sklearn.preprocessing import OneHotEncoder
 onehotencoder=OneHotEncoder(categorical_features= [0])
 X=onehotencoder.fit_transform(X).toarray()
 
-#fEncoding categorial variable for y
+#Encoding categorial variable for y
 from sklearn.preprocessing import LabelEncoder
 labelencoder_y = LabelEncoder()
 y= labelencoder_y.fit_transform(y)
+
+#Splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y)
+
+#Feature Scaling to make the variable in the same scale
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+
+
+
