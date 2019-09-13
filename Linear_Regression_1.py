0b34ea0aed2b2256a263ac10d1d16a16c81c126f
Ayush Pratap Singh <51674131+AyushSingh004@users.noreply.github.com>
2019-09-14 01:07:30 +0530

    Add files via upload

---
+++ Linear_Regression_1.py
@@ -0,0 +1,70 @@
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing datasets
+dataset=pd.read_csv('Salary_Data.csv')
+
+#importing the data
+X=dataset.iloc[:,0].values
+y=dataset.iloc[:,-1].values
+
+#making feature matrix
+X=np.c_[X,np.ones(30)]
+
+
+#Splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y)
+
+#fitting simple linear regression into the training set
+from sklearn.linear_model import LinearRegression
+lin=LinearRegression()
+lin.fit(X_train,y_train)
+
+#to determine the score of our dataset
+lin.score(X_train,y_train)
+lin.score(X_test,y_test)
+lin.score(X,y)
+
+#predicting the test set result
+y_pred= lin.predict(X_test)
+
+#making the co-ordinate of equal size
+y=np.c_[y,np.ones(30)]
+
+#Visualising of training set result
+plt.scatter(X_train,y_train,color="blue")
+plt.plot(X_train,lin.predict(X_train),color="red")
+plt.title('SALARY vs EXPERIENCE (TRAINING SET)')
+plt.xlabel('AGE')
+plt.ylabel('SALARY')
+plt.show()
+
+plt.scatter(X_test,y_test,color="blue")
+plt.plot(X_test,lin.predict(X_test),color="red")
+plt.title('SALARY vs EXPERIENCE (TRAINING SET)')
+plt.xlabel('AGE')
+plt.ylabel('SALARY')
+plt.show()
+
+plt.scatter(X,y,color="blue")
+plt.plot(X,lin.predict(X),color="red")
+plt.title('SALARY vs EXPERIENCE (TRAINING SET)')
+plt.xlabel('AGE')
+plt.ylabel('SALARY')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
