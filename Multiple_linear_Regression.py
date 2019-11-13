44fb66d1e48322d81281a956366a75e1f10f9b38
Ayush Pratap Singh <51674131+AyushSingh004@users.noreply.github.com>
2019-11-13 23:48:51 +0530

    Update Multiple_linear_Regression.py

--- Multiple_linear_Regression.py
+++ Multiple_linear_Regression.py
@@ -42,3 +42,22 @@ y_pred = lin_reg.predict(X_test)
 lin_reg.score(X_train,y_train)
 lin_reg.score(X_test,y_test)
 lin_reg.score(X,y)
+
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr= np.ones((50,1)).astype(int) , values=X , axis=1)
+X_opt = X[ : , [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog= y , exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[ : , [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog= y , exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[ : , [0,3,4,5]]
+regressor_OLS = sm.OLS(endog= y , exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[ : , [0,3,5]]
+regressor_OLS = sm.OLS(endog= y , exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[ : , [0,3]]
+regressor_OLS = sm.OLS(endog= y , exog= X_opt).fit()
+regressor_OLS.summary()
