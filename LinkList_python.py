156c75e37ecb5c713965ad500122df5dbc1ecf59
Ayush Pratap Singh <51674131+AyushSingh004@users.noreply.github.com>
2020-08-09 21:28:23 +0530

    Update LinkList_python.py

--- LinkList_python.py
+++ LinkList_python.py
@@ -5,29 +5,39 @@ class node:
 class linklist:
     def __init__(self):
         self.head=None
-    def traversal(self):
-        temp=self.head
-        while temp is not None:
-            print(temp.data)
-            temp=temp.next
-    def beginning(self,newdata):
+    def beg(self,newdata):
         newnode=node(newdata)
         newnode.next=self.head
         self.head=newnode
     def last(self,newdata):
         newnode=node(newdata)
-        while self.head is None:
+        if self.head is None:
             self.head=newnode
         temp=self.head
-        while(temp.next):
+        while temp.next is not None:
             temp=temp.next
         temp.next=newnode
+    def insertatposition(self,newdata,pos):
+        newnode=node(newdata)
+        position=0
+        temp=self.head
+        while temp is not None:
+            if position==pos:
+                previousnode.next=newnode
+                newnode.next=temp
+            previousnode=temp
+            temp=temp.next
+            position+=1
+    def printlist(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.data)
+            temp=temp.next
 l=linklist()
 l.head=node(1)
 second=node(2)
-third=node(3)
 l.head.next=second
-second.next=third
-l.beginning(0)
+l.beg(3)
 l.last(4)
-l.traversal()
+l.insertatposition(6,3)
+l.printlist()
